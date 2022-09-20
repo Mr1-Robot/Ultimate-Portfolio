09752e2148459adebbcc8d7fb8fe924253a8d9b1
Mr1-Robot <robotmr969@gmail.com>
2022-09-20 05:58:44 +0300

    Photo Edited.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/mrrobot1.png'
+import ME from '../../assets/me.png'
 import HeaderSocial from './HeaderSocial'
 import { Link } from 'react-scroll'
 
